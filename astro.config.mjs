155d6794f1f3a0e22b509c6ac1a9f303590352fd
Nicolás Torres L <nicolas.torreslop@gmail.com>
2025-02-24 23:21:39 -0500

    modify: deploy.yml pnpm version

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://d-ghoost.github.io',
+    site: 'https://D-Ghoost.github.io',
     base: 'el-rincon-de-fantasmin',
 });
