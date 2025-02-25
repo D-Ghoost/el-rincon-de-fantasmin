4ac93d219ef87d25d99b5b39e3bd74d528879143
Nicolás Torres L <nicolas.torreslop@gmail.com>
2025-02-24 22:48:20 -0500

    modify: astro config to gh pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: 'https://d-ghoost.github.io',
+    base: 'el-rincon-de-fantasmin',
+});
