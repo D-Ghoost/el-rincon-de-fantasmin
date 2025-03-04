1189c5bcf97b01a540cc517591c4219542630fce
Nicolás Torres L <nicolas.torreslop@gmail.com>
2025-03-03 19:28:03 -0500

    change: modify the main route

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,5 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
     site: 'https://D-Ghoost.github.io',
-    base: 'el-rincon-de-fantasmin',
+    base: '/',
 });
