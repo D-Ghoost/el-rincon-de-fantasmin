f708eb2b69ad3ddc039a61c9339d078b1e898d7e
Nicolás Torres L <nicolas.torreslop@gmail.com>
2025-03-19 10:11:26 -0500

    fix: route init project

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://D-Ghoost.github.io',
+    site: 'https://D-Ghoost.github.io/el-rincon-de-fantasmin/',
     base: '/',
 });
