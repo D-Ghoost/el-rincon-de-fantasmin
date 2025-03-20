4e7adfeeac7bc04a6e78488dac0539ead4219cd5
Nicolás Torres L <nicolas.torreslop@gmail.com>
2025-03-19 20:26:25 -0500

    fix: route base to deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://D-Ghoost.github.io/el-rincon-de-fantasmin/',
-    base: '/',
+    site: 'https://D-Ghoost.github.io',
+    base:'/el-rincon-de-fantasmin'
 });
--- src/const.ts
+++ src/const.ts
@@ -1,2 +1,3 @@
 export const SITE_TITLE = 'El Rincón de Fantasmin';
 export const SITE_DESCRIPTION = 'Bienvenido al Rincon de un fantasma que intenta vivir de nuevo';
+export const SITE_BASE = import.meta.env.BASE_URL;
