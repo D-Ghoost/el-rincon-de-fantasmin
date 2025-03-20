97f9bdc33be19ef0ef6020a130ac29cb3018028e
Nicolás Torres L <nicolas.torreslop@gmail.com>
2025-03-19 20:48:10 -0500

    fix: style to blog post

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,6 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
     site: 'https://D-Ghoost.github.io',
-    base:'/el-rincon-de-fantasmin'
+    base:'/el-rincon-de-fantasmin',
+    output: 'static'
 });
